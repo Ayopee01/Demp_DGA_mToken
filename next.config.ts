763919ce144333841ffb6674134374a82d459517
Ayopee <Ayopee001@gmail.com>
2025-12-18 16:23:06 +0700

    update

--- next.config.ts
+++ next.config.ts
@@ -3,9 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
-  // basePath: "/test2",
-  // assetPrefix: "/test2",
-  // output: "standalone"
+  basePath: "/test2",
+  assetPrefix: "/test2",
+  output: "standalone"
 };
 
 export default nextConfig;
